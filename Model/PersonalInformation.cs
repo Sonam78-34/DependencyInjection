cf6953b090303389c01a18f0a0d50f94cc47f9bd
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-06-05 09:59:33 +0530

    Add files via upload

---
+++ Model/PersonalInformation.cs
@@ -0,0 +1,10 @@
+﻿namespace DependencyInjection.Model
+{
+    public class PersonalInformation
+    {
+        public string Name { get; set; }
+        public string department { get; set; }  
+         
+        public string city { get; set; }
+    }
+}
